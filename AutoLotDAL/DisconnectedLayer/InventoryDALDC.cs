5b2ddf2eb4c263c15c84c0e2a93818493d8deab8
unknown <jakub.zezula@hotmail.com>
2023-07-24 23:32:19 +0200

    Add disconnected layer

---
+++ AutoLotDAL/DisconnectedLayer/InventoryDALDC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace AutoLotDAL.DisconnectedLayer
+{
+    public class InventoryDALDC
+    {
+        private string _connectionString;
+        private SqlDataAdapter _adapter = null;
+
+        public InventoryDALDC(string connectionString)
+        {
+            _connectionString = connectionString;
+
+            ConfigureAdapter(out _adapter);
+        }
+
+        public void ConfigureAdapter(out SqlDataAdapter adapter)
+        {
+            adapter = new SqlDataAdapter("Select * From Inventory", _connectionString);
+
+            var builder = new SqlCommandBuilder(adapter);
+        }
+    }
+}
